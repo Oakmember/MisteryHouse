bbc0d09bc36db655317ed171ff0a23bb6d324d2e
tzackiewicz <pogtoma@gmail.com>
2022-11-14 08:48:31 +0100

    Upload the Unity project files

--- Source/UE5ExportToUnity/UE5ExportToUnity.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5ExportToUnity.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5ExportToUnity, "UE5ExportToUnity" );
